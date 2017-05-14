fcbc13e8e93b0a84baf6388d051234b424a48a6c
Froren <r@sager>
2017-05-14 04:20:00 -0400

    fixed the chat to give some rudimentary response

--- LawBuddy-website/chat-ui/js/chat-ui.js
+++ LawBuddy-website/chat-ui/js/chat-ui.js
@@ -6,6 +6,18 @@ const input = $('#userInput');
 // CREATE PUBNUB CONNECTION
 const pubnub = new PubNub({ publishKey : 'pub-c-793237f1-ee68-427e-874b-1a4363e8862b', subscribeKey : 'sub-c-9c152790-3825-11e7-b611-0619f8945a4f' });
 const channel = 'pubnub-law-analysis1';
+
+pubnub.addListener({
+  message: function(obj) {
+	  
+	  var message = "";
+	  if (obj.message && obj.message.outputTopics) {
+		 message = "Let me search for some of the related topics I found: " + obj.message.outputTopics;
+	  }
+	  
+	  appendMessage(message);
+  }});
+
 pubnub.subscribe({channels:[channel]});
 
 $('.left .person').mousedown(function(){
@@ -21,22 +33,26 @@ $('.left .person').mousedown(function(){
         $('.chat[data-chat = '+findChat+']').addClass('active-chat');
     }
 });
+
+	function appendMessage(message) {
+		var chatBox = document.getElementById('chat');
+		var userBubble = document.createElement('div');
+		userBubble.className += "bubble me";
+		var userBubbleText = document.createTextNode(message);
+		userBubble.appendChild(userBubbleText);
+		chatBox.appendChild(userBubble);
+	}
+	
 // PUBLISHES THE USER'S INPUT IN THE CHAT BOX
   function publishUserMsg() {
-    var userMessageInput = $('#userInput').val(),
-    chatBox = document.getElementById('chat'),
-    userBubble = document.createElement('div');
-    userBubble.className += "bubble me";
-    var userBubbleText = document.createTextNode(userMessageInput);
-    userBubble.appendChild(userBubbleText);
-    chatBox.appendChild(userBubble);
-    userMessageInput.value = '';
+    var userMessageInput = $('#userInput').val();
+	appendMessage(userMessageInput);
+    $('#userInput').val('');
 // PUBLISHES MESSAGE TO PUBNUB
     pubnub.publish({channel: channel, message : {text: userMessageInput}});
   }
 
-  input.addEventListener('keyup', function(e) {
-    var userMessageInput = $('#userInput').val();
+  input.bind('keyup', function(e) {
     if ((e.keyCode || e.charCode) === 13)
         publishUserMsg();
   });
